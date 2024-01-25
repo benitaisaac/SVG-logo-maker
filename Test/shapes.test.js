405c2944f9b3cce0f9d97ecf170a549e03c12f04
benitaisaac <benita.isaac@gmail.com>
2024-01-24 17:50:39 -0800

    application sucessfully passed triangle test

--- Test/shapes.test.js
+++ Test/shapes.test.js
@@ -4,10 +4,30 @@ const Circle = require('../lib/circle.js');
 const Square = require('../lib/square.js');
 
 describe('Shape', () => {
+    
+    // const texts = ['ABC', 'uuu', 'apz', 'pqr', 'XYZ'];
+
+
+    // test.each(texts)(
+    //     "passes for text %j",
+    //     (fixture) => expect(triangle.renderSvg()).toEqual(result)
+    
+
+    //     const result = `<svg version="1.1"
+    //     width="300" height="200"
+    //     xmlns="http://www.w3.org/2000/svg">
+    
+    // <polygon points="150 0, 0 200, 300 200" fill="red" />
+    
+    // <text x="150" y="125" font-size="60" text-anchor="middle" fill="blue">${fixture}</text>
+    
+    // </svg>`
+       
+    // );
+    
+
     test('should render the correct svg string info', () => {
       const triangle = new Triangle('abc', 'blue', 'red');
-    //   const circle = new Circle(())
-      const err = new Error('Child class must implement a render() method.')
       const result = `<svg version="1.1"
         width="300" height="200"
         xmlns="http://www.w3.org/2000/svg">
