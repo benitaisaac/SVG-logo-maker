70721a778b8c424d2b3af202a6c5cb86dcd4d1b7
benitaisaac <benita.isaac@gmail.com>
2024-01-24 17:56:34 -0800

    created sucessful circle test

--- Test/shapes.test.js
+++ Test/shapes.test.js
@@ -26,7 +26,7 @@ describe('Shape', () => {
     // );
     
 
-    test('should render the correct svg string info', () => {
+    test('should render the correct triangle svg string', () => {
       const triangle = new Triangle('abc', 'blue', 'red');
       const result = `<svg version="1.1"
         width="300" height="200"
@@ -40,3 +40,21 @@ describe('Shape', () => {
       expect(triangle.renderSvg()).toEqual(result);
     });
   });
+
+
+describe('Shape', () => {
+    test('should render correct circle svg string', () => {
+        const circle = new Circle('acb', 'purple', 'blue');
+        const result = `<svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+
+<circle cx="150" cy="100" r="80" fill="blue" />
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="purple">acb</text>
+
+</svg>`
+expect(circle.renderSvg()).toEqual(result);
+    });
+});
+
