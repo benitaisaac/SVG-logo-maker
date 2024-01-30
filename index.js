b997625f3c5b84ce51964b88bb3d970562cfe3a1
benitaisaac <benita.isaac@gmail.com>
2024-01-30 10:16:21 -0800

    cleaned up code, got rid of comments

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ const questions = [
         message: 'What color do you want the shape to be?',
     }
 ]
-
-//TO DO: have this function create an SVC file and populate answers in there 
+ 
 inquirer
 .prompt(questions)
 
@@ -62,57 +61,5 @@ switch(answers.shape){
         console.log('Saved!');
       });
 
-
-
-    // let mm = new Shape('ABC', 'white', 'red', 'square');
-
-    //console.log(mm);
-
-    // const myShape = new Shape(answers.text, answers.textColor, answers.shapeColour, answers.shape);
-    // console.log(myShape.render());
-    // fs.writeFile('logo.svg', myShape.render(), function (err) {
-    //         if (err) throw err;
-    //         console.log('Saved!');
-    //       });
-    // fs.writeFile('input.JSON', JSON.stringify(answers, null, '  '));
-    //return JSON.stringify(answers, null, '  ');
-
     
 } );
-
-// fs.writeFile('logo.svg', myShape.render(), function (err) {
-    //     if (err) throw err;
-    //     console.log('Saved!');
-    //   });
-
-
-
-
-// .then((answers) => {
-//     fs.writeFile('README.md', generateREADME(answers), (err) => {
-//         if (err) throw err;
-//         console.log('The README was created!');
-//     })
-//   });
-
-
-//Acceptance Criteria: 
-//GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
